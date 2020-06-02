3b72bbe2befc8943dedc906c796b71e0ece571ca
Fill <happyhellakasacred@gmail.com>
2020-06-03 02:56:40 +0700

    completed lesson 4, upd PostController, add show.html.twig

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -1,29 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
-    /**
-     * @Route("/posts", name="posts")
-     */
-    public function index()
+    /** @var PostRepository $postRepository */
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
     {
-        $posts = [
-            'post_1' => [
-                'title' => 'Заголовок первого поста',
-                'body' => 'Тело первого поста'
-            ],
-            'post_2' => [
-                'title' => 'Заголовок второго поста',
-                'body' => 'Тело второго поста'
-            ]
-        ];
+        $this->postRepository = $postRepository;
+    }
+
+    /**
+     * @Route("/posts", name="blog_posts")
+     */
+    public function posts()
+    {
+        $posts = $this->postRepository->findAll();
+
         return $this->render('posts/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/posts/{slug}", name="blog_show")
+     */
+    public function post(Post $post)
+    {
+        return $this->render('posts/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
